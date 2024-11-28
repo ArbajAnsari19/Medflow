3aaa67ff438728ac574ce1b1c06fe09a6e718cc1
Arbaj Ansari <127431775+ArbajAnsari19@users.noreply.github.com>
2024-11-28 22:08:11 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,11 +8,13 @@ const orderRouter = require("./Routes/orderRoutes");
 
 const app = express();
 
-app.use(cors());
 
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.get("/test", (req, res) => {
   res.send("Hello World");
 });
